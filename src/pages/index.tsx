35b31fe433f089c7502ae3f46ff2f5aedf1fedb3
pbennettwa <pbennettwa@gmail.com>
2021-10-22 15:56:05 -0500

    added basic styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import { useState } from 'react';
-import {
-  useAppDispatch,
-  useAppSelector,
-} from '../app/hooks';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
 import {
   decrement,
   increment,
@@ -10,34 +7,36 @@ import {
   selectCount,
 } from '../features/counter/counterSlice';
 
-const IndexPage:React.FC = () => {
+const IndexPage: React.FC = () => {
   const dispatch = useAppDispatch();
   const count = useAppSelector(selectCount);
   const [incrementAmount, setIncrementAmount] = useState<number>(0);
 
   return (
-    <>
+    <div className='app'>
       <h1>Hello World!</h1>
-      <h2>
-        The current number is {count}
-      </h2>
+      <h2>The current number is {count}</h2>
       <div>
         <input
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(Number(e.target.value))}
-          type="number"
+          type='number'
+          className='input'
         />
+        &nbsp;
         <button
-          onClick={() =>        dispatch(incrementByAmount(Number(incrementAmount)))}
+          onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
         >
           Increment by amount
         </button>
       </div>
       <div>
+        <div>&nbsp;</div>
         <button onClick={() => dispatch(decrement())}>Decrement by 1</button>
+        &nbsp;
         <button onClick={() => dispatch(increment())}>Increment by 1</button>
       </div>
-    </>
+    </div>
   );
 };
 
